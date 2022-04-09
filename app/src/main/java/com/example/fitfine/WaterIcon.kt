6487f5dad61266aae659f9e82ab1ee60c2310ca1
arsalanahmad4 <nurulhudakhan.567@gmail.com>
2022-04-09 20:28:06 +0530

    Second Commit

---
+++ app/src/main/java/com/example/fitfine/WaterIcon.kt
@@ -0,0 +1,11 @@
+package com.example.fitfine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaterIcon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_water_icon)
+    }
+}
